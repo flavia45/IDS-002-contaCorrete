71d6eb962a622e05b837959389db89851bd04e2a
lab3.4 <lab34@fatec.com>
2023-11-09 22:00:48 -0300

    finaliza exercicio

---
+++ contaCorrete/src/main/java/com/fatecbs/contaCorrete/ContaCorreteApplication.java
@@ -0,0 +1,13 @@
+package com.fatecbs.contaCorrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaCorreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaCorreteApplication.class, args);
+	}
+
+}
---
+++ contaCorrete/src/test/java/com/fatecbs/contaCorrete/ContaCorreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecbs.contaCorrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaCorreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
